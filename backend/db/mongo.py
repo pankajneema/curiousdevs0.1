6f8f536f912d12057f5f78bf8fbc51d0cf57bf3f
Pankaj neema <80908951+pankajneema@users.noreply.github.com>
2025-11-20 07:12:48 +0530

    Fix MongoDB URI encoding issue

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -1,5 +1,5 @@
 from pymongo import AsyncMongoClient
 from decouple import config
 
-client = AsyncMongoClient(config("MONGO_URI", default="mongodb+srv://pankaj200321:Pankaj@curious1@pankajcluster.wkn8k.mongodb.net/?appName=pankajcluster"))
+client = AsyncMongoClient(config("MONGO_URI", default="mongodb+srv://pankaj200321:Pankaj%40curious1@pankajcluster.wkn8k.mongodb.net/?appName=pankajcluster"))
 db = client["project_management"]
