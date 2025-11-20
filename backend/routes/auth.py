b80ecff87924e034734bd9a3506d39ad74b52f7b
Pankaj neema <80908951+pankajneema@users.noreply.github.com>
2025-11-20 22:10:41 +0530

    Refactor auth.py for improved clarity and functionality

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, HTTPException
 from models.user import UserCreate, UserLogin
 from db.mongo import db
 from passlib.hash import bcrypt_sha256
@@ -11,7 +11,6 @@ router = APIRouter(prefix="/auth")
 async def signup(user: UserCreate):
     # Check if user already exists
     exists = await db.users.find_one({"email": user.email})
-    print(f"User Result : {exists}")
 
     if exists:
         return {
@@ -19,20 +18,40 @@ async def signup(user: UserCreate):
             "message": "Email already registered"
         }
 
-    # Convert to dict
+    # Convert Pydantic model to dict
     user_dict = user.dict()
 
-    # Hash password safely (no 72-byte limit issue)
+    # Hash password
     user_dict["password"] = bcrypt_sha256.hash(user.password)
 
     user_dict["role"] = "customer"
+
     await db.users.insert_one(user_dict)
-    return {"status":"success","message": "User created sucessfully"}
+
+    return {
+        "status": "success",
+        "message": "User created successfully"
+    }
+
 
 @router.post("/login")
 async def login(user: UserLogin):
     found = await db.users.find_one({"email": user.email})
-    if not found or not bcrypt.verify(user.password, found["password"]):
-        return {"status":"failed","message": "Email not found"}
-    token = create_token({"user_id": str(found["_id"]), "role": found["role"]})
-    return {"status":"success","token": token}
+
+    # Incorrect email or wrong password
+    if not found or not bcrypt_sha256.verify(user.password, found["password"]):
+        return {
+            "status": "failed",
+            "message": "Invalid email or password"
+        }
+
+    # Create JWT token
+    token = create_token({
+        "user_id": str(found["_id"]),
+        "role": found["role"]
+    })
+
+    return {
+        "status": "success",
+        "token": token
+    }
