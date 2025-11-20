18281204b219dcfdcdc70601eba1c9354be0f5be
Pankaj neema <80908951+pankajneema@users.noreply.github.com>
2025-11-20 07:22:47 +0530

    Update password hashing to bcrypt_sha256

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,21 +1,30 @@
-from fastapi import APIRouter,status, HTTPException
-from models.user import UserCreate ,UserLogin
+from fastapi import APIRouter, status, HTTPException
+from models.user import UserCreate, UserLogin
 from db.mongo import db
-from passlib.hash import bcrypt
+from passlib.hash import bcrypt_sha256
 from utils.auth import create_token
 from fastapi.responses import JSONResponse
-
 
 router = APIRouter(prefix="/auth")
 
 @router.post("/signup")
 async def signup(user: UserCreate):
+    # Check if user already exists
     exists = await db.users.find_one({"email": user.email})
     print(f"User Result : {exists}")
+
     if exists:
-        return {"status": "failed", "message": "Email already registered"}
+        return {
+            "status": "failed",
+            "message": "Email already registered"
+        }
+
+    # Convert to dict
     user_dict = user.dict()
-    user_dict["password"] = bcrypt.hash(user.password)
+
+    # Hash password safely (no 72-byte limit issue)
+    user_dict["password"] = bcrypt_sha256.hash(user.password)
+
     user_dict["role"] = "customer"
     await db.users.insert_one(user_dict)
     return {"status":"success","message": "User created sucessfully"}
